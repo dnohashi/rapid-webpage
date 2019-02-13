a232ad24006f526347c897041482dbbdd82f799c
Dean Ohashi <dnohashi@Deans-MacBook-Pro.local>
2019-02-13 14:09:15 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a front-end mock design of a digital agency website. 
---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 0){
+    document.querySelector("#navbar").style.background = "rgba(0, 0, 0, 1)";
+  } else {
+    this.document.querySelector('#navbar').style.background = "rgba(0, 0, 0, 0)";
+  }
+})
+
+
+//https://demo.athemes.com/airi-agency/
